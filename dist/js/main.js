e4910244177ea2ffae84a0cfd0272aa9c06c6b26
abuna1985 <adamjabundis@gmail.com>
2019-04-08 03:07:45 -0700

    Update css and javascript files

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,42 +1,63 @@
-// Select DOM Items
-const menuBtn = document.querySelector(".menu-btn");
-const menu = document.querySelector(".menu");
-const menuBranding = document.querySelector(".menu__branding");
-const menuNav = document.querySelector(".menu-nav");
-const navItems = document.querySelectorAll(".menu-nav__item");
+"use strict";
 
-// Set Initial State of the Menu
-let isMenuShown = false;
-
-document.addEventListener(
-  "click",
-  function(e) {
-    if (e.target.closest(".menu-btn")) {
-      e.preventDefault();
-      toggleMenu();
-    }
-  },
-  false
-);
-
-function toggleMenu() {
-  if (!isMenuShown) {
-    menuBtn.classList.add("menu--close");
-    menu.classList.add("menu--show");
-    menuNav.classList.add("menu--show");
-    menuBranding.classList.add("menu--show");
-    navItems.forEach(item => item.classList.add("menu--show"));
-
-    // Set Menu State
-    isMenuShown = true;
-  } else {
-    menuBtn.classList.remove("menu--close");
-    menu.classList.remove("menu--show");
-    menuNav.classList.remove("menu--show");
-    menuBranding.classList.remove("menu--show");
-    navItems.forEach(item => item.classList.remove("menu--show"));
-
-    // Set Menu State
-    isMenuShown = false;
+var Modal =
+/*#__PURE__*/
+function () {
+  //constructor
+  function Modal() {
+    // Select DOM Items
+    this.menuBtn = document.querySelector(".menu-btn");
+    this.menu = document.querySelector(".menu");
+    this.menuBranding = document.querySelector(".menu__branding");
+    this.menuNav = document.querySelector(".menu-nav");
+    this.navItems = document.querySelectorAll(".menu-nav__item"); 
+    // Set Initial State of the Menu
+    this.isMenuShown = false;
+    //create event listener
+    this.event();
   }
-}
+
+  var _proto = Modal.prototype;
+
+  //events function
+  _proto.event = function event() {
+    var _this = this;
+
+    document.addEventListener("click", function (e) {
+      if (e.target.closest(".menu-btn")) {
+        e.preventDefault();
+
+        _this.toggleMenu();
+      }
+    }, false);
+  };
+
+  //custom functions
+  _proto.toggleMenu = function toggleMenu() {
+    if (!this.isMenuShown) {
+      this.menuBtn.classList.add("menu--close");
+      this.menu.classList.add("menu--show");
+      this.menuNav.classList.add("menu--show");
+      this.menuBranding.classList.add("menu--show");
+      this.navItems.forEach(function (item) {
+        return item.classList.add("menu--show");
+      }); // Set Menu State
+
+      this.isMenuShown = true;
+    } else {
+      this.menuBtn.classList.remove("menu--close");
+      this.menu.classList.remove("menu--show");
+      this.menuNav.classList.remove("menu--show");
+      this.menuBranding.classList.remove("menu--show");
+      this.navItems.forEach(function (item) {
+        return item.classList.remove("menu--show");
+      }); // Set Menu State
+
+      this.isMenuShown = false;
+    }
+  };
+
+  return Modal;
+}();
+
+var modal = new Modal();
